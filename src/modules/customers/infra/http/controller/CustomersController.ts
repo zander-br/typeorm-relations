fa1f75732e4ecb4a0e838b5a0a3c6f43a85b1652
Anderson Santos <anderson.feitoza.santos@gmail.com>
2020-11-25 06:01:43 -0300

    feat: add CustomersController

--- src/modules/customers/infra/http/controller/CustomersController.ts
+++ src/modules/customers/infra/http/controller/CustomersController.ts
@@ -1,11 +1,15 @@
 import { Request, Response } from 'express';
+import { container } from 'tsyringe';
 
 import CreateCustomerService from '@modules/customers/services/CreateCustomerService';
 
-import { container } from 'tsyringe';
-
 export default class CustomersController {
   public async create(request: Request, response: Response): Promise<Response> {
-    // TODO
+    const { name, email } = request.body;
+    const createCustomerService = container.resolve(CreateCustomerService);
+
+    const customer = await createCustomerService.execute({ name, email });
+
+    return response.json(customer);
   }
 }
